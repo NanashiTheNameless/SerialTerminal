149156e3c35322c09df1057a0d3d7f83d0f4b1be
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-08-09 04:41:44 -0500

    oopsie

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Box from '@mui/material/Grid'
+import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
 import Alert from '@mui/material/Alert'
